cdb84f0d5d7a650d1c57ce147e39f1197ca4a621
johnnynuss10 <johnnynuss10@gmail.com>
2026-07-10 18:37:37 -0400

    added buttons for individual nodes
    added buttons for viewing telemetry on a per node basis, after entering valid device ID

--- script.js
+++ script.js
@@ -1,11 +1,50 @@
-//let msgOut = "ESP32_ad025C"
 let msgOut = null 
 let visibleNodes = [];
+let numNodes = 0;
+
+async function ListNodes(){
+
+    const payload ={
+        deviceID : msgOut
+    };
+    try{
+        const response = await fetch("https://z4wpd0rrrb.execute-api.us-east-1.amazonaws.com/ListNodes/POST", {
+            method : "POST",
+            headers : {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(payload)
+        });
+
+        if (!response.ok){
+            throw new Error(`Response status: ${response.status}`);
+        }
+        const result = await response.json();
+        console.log(result);
+
+        // Create a new button for each node found 
+        numNodes = Object.keys(result).length;
+        const keys = Object.keys(result);
+        for (let i = 0; i < numNodes; i++){
+                visibleNodes[i] = document.createElement("button");
+                visibleNodes[i].textContent = `Node ${keys[i]}`;
+                document.body.appendChild(visibleNodes[i]);
+            }
+        console.log(numNodes);
+
+    }
+    catch (error) {
+        console.error(error);
+    }
+
+
+}
+
 async function displayCustomText(){
     
     // take msgOut and send it to aws lambda
     const payload = {
-        name: msgOut
+        deviceID: msgOut
     };
 
     // fetch request here using POST method
@@ -20,7 +59,7 @@ async function displayCustomText(){
 
         if (!response.ok){
             throw new Error(`Response status: ${response.status}`);
-    }
+        }
         const result = await response.json();
         // identify nodes in response
         document.getElementById("output").textContent =
@@ -38,17 +77,26 @@ function printTextBox(){
         msgOut = document.getElementById("deviceIdFromForm").value;
         console.log("New msgOut: ", msgOut);
 
-        displayCustomText();
+        // check for previously placed node buttons and delete them
+        if (visibleNodes[0]){
+            for (let i = 0; i < numNodes; i++) {
+                visibleNodes[i].remove();
+            }
+        }
+        numNodes = 0;
+
+        //displayCustomText();
+        ListNodes();
     })
     
 
 }
+
+
 
 printTextBox();
 
 
 //displayCustomText();
 
-
-//setInterval(printTextBox, 100);
 
